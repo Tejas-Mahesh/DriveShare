b3140ec1cd8c80a0c9ea322a4eb6f1bdf75841e2
Pavan Kumar M <tejasmanjulakg@gmail.com>
2026-08-30 12:41:04 +0530

    Fix Render deployment dependencies

--- DriveShare/settings.py
+++ DriveShare/settings.py
@@ -348,13 +348,7 @@ LOGOUT_REDIRECT_URL = "home"
 # RAZORPAY
 # ============================================================
 
-RAZORPAY_KEY_ID = os.getenv(
-    "RAZORPAY_KEY_ID"
-)
 
-RAZORPAY_KEY_SECRET = os.getenv(
-    "RAZORPAY_KEY_SECRET"
-)
 
 
 # ============================================================
